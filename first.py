eb5fcb793fa6e3647ddac66918d2f233d7dd74f1
Prajwal B S <prajwaljhon@gmail.com>
2024-12-03 15:18:31 +0530

    Basic Defination and It's features

--- first.py
+++ first.py
@@ -1,9 +1,9 @@
 import requests
-url = 'https://github.com/harshadvali11/complete_wheather_app/blob/main/app/views.py'
+url = 'https://jsonplaceholder.typicode.com/todos/1'
 response = requests.get(url)
 # Handling the response object
 if response.status_code == 200:
     print('Request successful!')
-    print('Response Content:', response.headers())
+    print('Response Content:', response.json())
 else:
     print('Request failed with status code:', response.status_code)
