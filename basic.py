e1941ee04ce4aa8e9ffc615a1efcb57ed468aea8
Prajwal B S <prajwaljhon@gmail.com>
2025-01-04 15:56:26 +0530

    Binary Response Content

--- basic.py
+++ basic.py
@@ -3,4 +3,14 @@ import requests
 # print(r.cookies)
 payload = {'Name': 'Jhon', 'Password': 'Prajwal'}
 r = requests.get('https://httpbin.org/get', params=payload)
+# print(r.text)
+print(r.encoding)
+r.encoding = 'ISO-8859-1'
 print(r.text)
+print(r.encoding)
+from PIL import Image
+from io import BytesIO
+
+# i = Image.open(BytesIO(r.content))
+# print(i)
+print(r.content)
